06d3899f8c47f5e44583afd89dd45c4ad5e4cea4
Minoord <minoord18@outlook.com>
2021-09-17 10:53:28 +0200

    Added Les 1

---
+++ Les1/SYSEnemy/SYSEnemy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SYSEnemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy EnemyHealth = new Enemy();
+            bool Alive = true;
+
+            while (Alive)
+                {
+                    if (EnemyHealth.Health == 1)
+                    {
+                        Alive = false;
+                    }
+                    EnemyHealth.ChangeHp();
+                    Console.WriteLine(EnemyHealth.Health);
+                }
+
+        }
+
+    }
+
+    class Enemy
+    {
+        public  int Health = 10;
+        public  int DamageToEnemy(int EnemyHp, int Damage)
+        {
+            EnemyHp -= Damage;
+            return EnemyHp;
+        }
+        public int ChangeHp() 
+        {
+            Health = DamageToEnemy(Health, 1);
+            return Health;
+        }
+    }
+}
