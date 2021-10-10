ea0e407412c8bef62df66058306a62adddfe9a1f
Minoord <minoord18@outlook.com>
2021-10-10 15:36:43 +0200

    Added Les 4

---
+++ Les4/SpriteInheritance/SpriteInheritance/SirSlippy.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace Eindopdracht
+{
+    class SirSlippy : Sprite
+    {
+
+        public SirSlippy(RectangleF screenPosition)
+        {
+            this.screenPosition = screenPosition;
+            imageFrame = new Rectangle(82, 801, 16, 16);
+        }
+    }
+}
+
+
+
